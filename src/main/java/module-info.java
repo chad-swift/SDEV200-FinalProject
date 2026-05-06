f3c28e01147ccc97711f09e9d47309ff06247614
Chad Swift <165844586+chad-swift@users.noreply.github.com>
2026-05-05 20:28:38 -0400

    added new module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.sdev200finalproject {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens org.example.sdev200finalproject to javafx.fxml;
